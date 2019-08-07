18050a7cad4ea1311f2aac809b947584a3d5e578
riyanchhi <53818109+riyanchhi@users.noreply.github.com>
2019-08-07 13:15:14 +0530

    Add files via upload

---
+++ sorta.cpp
@@ -0,0 +1,33 @@
+/*Lab-0-Additional*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+freopen("input.txt","r",stdin);
+int n,t,key,a[10];
+cin>>t;
+cin>>n>>key;
+for(int j=0;j<t;j++)
+{
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+		
+	}
+	}	for(int i=0;i<n;i++){
+		
+		if(key==a[i])
+			break;
+}cout<<"1\n";
+
+
+		for(int i=0;i<n;i++){
+		
+		if(key=!a[i])
+			break; 
+}cout<<"-1\n";
+
+}
+
+
